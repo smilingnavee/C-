cdaf97682793def098109903c85fd3d4800fc508
Naveena Naganaboina <smilingnavee@gmail.com>
2017-09-21 15:28:37 +0530

    Update Function_Template.cpp

--- Function_Template.cpp
+++ Function_Template.cpp
@@ -1,1 +1,12 @@
-
+/*
+A function template is a function which contains generic code to operate on different types of data. 
+This enables a programmer to write functions without having to specify the exact type of parameters.
+Syntax for defining a template function is as follows:
+      template<class Type, ...>
+      return–type function–name(Type arg1, ...)
+      {
+      //Body of function template
+      }
+keyword template followed by a list of template type arguments or also called generic arguments.
+The template keyword tells the compiler that what follows is a template. Here, class is a keyword and Type is the name of generic argument.
+*/
